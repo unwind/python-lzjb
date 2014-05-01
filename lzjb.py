8d85d404fde2e652193f5627b43d00a247eda545
Emil Brink <emil@obsession.se>
2014-05-01 16:44:04 +0200

    Implemented encoding of the input size.

--- lzjb.py
+++ lzjb.py
@@ -3,8 +3,8 @@
 # An attempt at re-implementing LZJB compression in native Python.
 #
 
+import cStringIO
 import math
-
 
 NBBY = 8
 MATCH_BITS = 6
@@ -13,12 +13,14 @@ MATCH_MAX = ((1 << MATCH_BITS) + (MATCH_MIN - 1))
 OFFSET_MASK = ((1 << (16 - MATCH_BITS)) - 1)
 LEMPEL_SIZE_BASE = 1024
 
-
 class Instream(object):
 	def __init__(self, s):
 		self._string = s
 		self._pos = 0
 		self._unbuffer = []
+
+	def size(self):
+		return len(self._string)
 
 	def get(self):
 		if len(self._unbuffer) > 0:
@@ -32,6 +34,7 @@ class Instream(object):
 	def push(self, char):
 		self._unbuffer.extend(char)
 
+
 def compress(s, compression = 1, c_compatible = True):
 	LEMPEL_SIZE = LEMPEL_SIZE_BASE
 	EXPAND = True
@@ -44,15 +47,23 @@ def compress(s, compression = 1, c_compatible = True):
 		if compression >= 2 and compression <= 4:
 			EXPAND += 1
 
-	out = ""
+	out = cStringIO.StringIO()
 	src = Instream(s)
-	while True:
-		c = src.get()
-		if c is None:
-			break
-		print c
-	return out
+
+	# Encode input size.
+	size = src.size()
+	if size:
+		sbytes = []
+		size += 1
+		while True:
+			sbytes.append(size & 0x7f)
+			size = int(math.floor(size / 128))
+			if size == 0:
+				break
+		sbytes[0] |= 0x80
+		for sb in reversed(sbytes):
+			out.write(chr(sb))
+		print out.getvalue()
 
 if __name__ == "__main__":
-	for c in xrange(1, 9):
-		compress("whatever ever is what this ever?", c)
+	compress(18 * "whatever ever is what this ever?")
