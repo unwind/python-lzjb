16b0d97c551c06d2bafddaa322f4dc76a5ee5ea8
Emil Brink <emil@obsession.se>
2014-05-01 16:26:52 +0200

    Implemented an Instream class to give push-back capability.
    No idea about the performance implications of this code.

--- lzjb.py
+++ lzjb.py
@@ -5,12 +5,32 @@
 
 import math
 
+
 NBBY = 8
 MATCH_BITS = 6
 MATCH_MIN = 3
 MATCH_MAX = ((1 << MATCH_BITS) + (MATCH_MIN - 1))
 OFFSET_MASK = ((1 << (16 - MATCH_BITS)) - 1)
 LEMPEL_SIZE_BASE = 1024
+
+
+class Instream(object):
+	def __init__(self, s):
+		self._string = s
+		self._pos = 0
+		self._unbuffer = []
+
+	def get(self):
+		if len(self._unbuffer) > 0:
+			return self._unbuffer.pop()
+		if self._pos < len(self._string):
+			r = self._string[self._pos]
+			self._pos += 1
+			return r
+		return None
+
+	def push(self, char):
+		self._unbuffer.extend(char)
 
 def compress(s, compression = 1, c_compatible = True):
 	LEMPEL_SIZE = LEMPEL_SIZE_BASE
@@ -23,7 +43,14 @@ def compress(s, compression = 1, c_compatible = True):
 			EXPAND = round(EXPAND * 1.5)
 		if compression >= 2 and compression <= 4:
 			EXPAND += 1
+
 	out = ""
+	src = Instream(s)
+	while True:
+		c = src.get()
+		if c is None:
+			break
+		print c
 	return out
 
 if __name__ == "__main__":
