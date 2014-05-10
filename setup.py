262cdcb439f36a78b65303e312378c34d1039b29
Emil Brink <emil@obsession.se>
2014-05-10 18:25:15 +0200

    More tweaks, trying to silence setup.py warnings.
    Only tested the "sdist" command, so far. No idea if the package is installable.

--- setup.py
+++ setup.py
@@ -5,9 +5,14 @@
 from distutils.core import setup
 setup(name = 'lzjb',
 	version = '1.0',
-	description = "A pure Python implementation of LZB compression/decompression",
+	description = "A pure Python implementation of LZJB compression/decompression",
 	author = "Emil Brink",
 	author_email = "emil@obsession.se",
 	url = "https://github.com/unwind/python-lzjb",
+	license = "BSD 2-clause",
 	py_modules = ['lzjb'],
+
+	classifiers = [
+	"License :: OSI Approved :: BSD License",
+	],
 	)
