18bf9a2c4401a3c8d4dc72158b9efade72ea8b88
Emil Brink <emil@obsession.se>
2014-05-10 14:55:05 +0200

    More tweaks of the setup script.
    Renamed README, let's see if this is GitHub-compatible.

--- setup.py
+++ setup.py
@@ -4,9 +4,10 @@
 
 from distutils.core import setup
 setup(name = 'lzjb',
+	version = '1.0',
 	description = "A pure Python implementation of LZB compression/decompression",
 	author = "Emil Brink",
 	author_email = "emil@obsession.se",
-	version = '1.0',
+	url = "https://github.com/unwind/python-lzjb",
 	py_modules = ['lzjb'],
 	)
