ccd7ad2db7b571d3960b0ef56c0576ccc48a4905
Emil Brink <emil@obsession.se>
2014-05-10 14:51:00 +0200

    Started on setup.py.
    Little to no idea what I'm doing here.

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+#
+#
+
+from distutils.core import setup
+setup(name = 'lzjb',
+	description = "A pure Python implementation of LZB compression/decompression",
+	author = "Emil Brink",
+	author_email = "emil@obsession.se",
+	version = '1.0',
+	py_modules = ['lzjb'],
+	)
