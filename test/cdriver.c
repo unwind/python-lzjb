8cf1e7b475acb12c4a1591703cb65cebe4477a6c
Emil Brink <emil@obsession.se>
2014-05-13 22:25:45 +0200

    Added size-handling, and implemented decompression.

--- test/cdriver.c
+++ test/cdriver.c
@@ -82,18 +82,74 @@ static bool save_file(const char *filename, const void *data, size_t length)
 	return wrote;
 }
 
+/* Encode the size in the given buffer. The buffer is big enough. */
+static void * size_put(void *buffer, size_t length)
+{
+	uint8_t *put = buffer;
+	length += 1;
+	do
+	{
+		*put++ = length & 0x7f;
+		length >>= 7;
+	} while(length != 0);
+	put[-1] |= 0x80;
+
+	return put;
+}
+
+/* Decode size from the given buffer. */
+static const void * size_get(const void *buffer, size_t *size)
+{
+	const uint8_t *get = buffer;
+	size_t out = 0, shift = 0;
+	bool busy = true;
+
+	while(busy)
+	{
+		uint8_t here = *get++;
+		if(here & 0x80)
+		{
+			here &= 0x7f;
+			busy = false;
+		}
+		out |= here << shift;
+		shift += 7;
+	}
+	if(size != NULL)
+		*size = out;
+	return get;
+}
+
 static void compress(const char *filename)
 {
-	printf("Compressing '%s' ...\n", filename);
 	size_t in_size;
 	void *in = load_file(filename, &in_size);
 	if(in != NULL)
 	{
 		const size_t out_max = in_size + 128;
 		void *out = malloc(out_max);
-		const size_t out_size = lzjb_compress(in, out, in_size, out_max, 0);
+		void *put = size_put(out, in_size);
+		const size_t out_left = out_max - ((unsigned char *) put - (unsigned char *) out);
+		const size_t out_size = lzjb_compress(in, put, in_size, out_left, 0);
 		if(save_file(filename, out, out_size))
 			printf("%-20s %zu -> %zu [%.1f%%]\n", filename, in_size, out_size, 100.f * out_size / in_size);
+		free(out);
+		free(in);
+	}
+}
+
+static void decompress(const char *filename)
+{
+	size_t in_size;
+	void *in = load_file(filename, &in_size);
+	if(in != NULL)
+	{
+		size_t out_size;
+		void *get = (void *) size_get(in, &out_size);
+		void *out = malloc(out_size);
+		lzjb_decompress(get, out, in_size, out_size, 0);
+		if(save_file(filename, out, out_size))
+			printf("%-20s %zu -> %zu [%.1f%%]\n", filename, in_size, out_size, 100.f * in_size / out_size);
 		free(out);
 		free(in);
 	}
@@ -119,6 +175,8 @@ int main(int argc, char *argv[])
 		}
 		else if(mode == COMPRESS)
 			compress(argv[i]);
+		else if(mode == DECOMPRESS)
+			decompress(argv[i]);
 	}
 	return EXIT_SUCCESS;
 }
