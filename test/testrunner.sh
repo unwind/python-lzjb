67ce39e2761b463e4100b40aafb29a16170e6324
Emil Brink <emil@obsession.se>
2014-05-19 21:56:22 +0200

    Fixed the find expression.
    Based on a Stack Overflow answer (http://stackoverflow.com/a/15066129/28169, +1:ed!).
    
    Also added -readable to get rid of permissions-based problems.

--- test/testrunner.sh
+++ test/testrunner.sh
@@ -6,7 +6,7 @@ DIR=$1
 PYDRIVER=../lzjb.py
 CDRIVER=./cdriver
 
-for f in $(find $DIR -type f -maxdepth 1 2> /dev/null | xargs ls -rS1 | head); do
+find $DIR -maxdepth 1  -type f  \! -name '.*'  -readable  -print0 | while IFS= read -r -d $'\0' f; do
 	./testfile.sh $PYDRIVER $PYDRIVER $f
 	./testfile.sh $PYDRIVER $CDRIVER  $f
 	./testfile.sh $CDRIVER  $PYDRIVER $f
