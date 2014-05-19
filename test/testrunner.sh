f47bbdd8274992eed4185feb9892297161b72095
Emil Brink <emil@obsession.se>
2014-05-19 22:08:30 +0200

    Cut off the 10 largest files and test only on those.

--- test/testrunner.sh
+++ test/testrunner.sh
@@ -6,7 +6,7 @@ DIR=$1
 PYDRIVER=../lzjb.py
 CDRIVER=./cdriver
 
-find $DIR -maxdepth 1  -type f  \! -name '.*'  -readable  -print0 | while IFS= read -r -d $'\0' f; do
+find $DIR -maxdepth 1  -type f  \! -name '.*'  -readable | head | xargs ls -rS | while IFS= read -r f; do
 	./testfile.sh $PYDRIVER $PYDRIVER $f
 	./testfile.sh $PYDRIVER $CDRIVER  $f
 	./testfile.sh $CDRIVER  $PYDRIVER $f
