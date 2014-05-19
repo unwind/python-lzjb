2ea368f85c5eb95574a4fce03c8930dc73350a3e
Emil Brink <emil@obsession.se>
2014-05-19 21:01:29 +0200

    Enable all three passes.

--- test/testrunner.sh
+++ test/testrunner.sh
@@ -7,7 +7,7 @@ PYDRIVER=../lzjb.py
 CDRIVER=./cdriver
 
 for f in $(find $DIR -type f -maxdepth 1 2> /dev/null | xargs ls -rS1 | head); do
-#	./testfile.sh $PYDRIVER $PYDRIVER $f
-#	./testfile.sh $PYDRIVER $CDRIVER  $f
+	./testfile.sh $PYDRIVER $PYDRIVER $f
+	./testfile.sh $PYDRIVER $CDRIVER  $f
 	./testfile.sh $CDRIVER  $PYDRIVER $f
 done
