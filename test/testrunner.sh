7c43121dfe2c580ca8bd5f5288caa6003ded1ec6
Emil Brink <emil@obsession.se>
2014-05-19 22:09:39 +0200

    Largest is not the same as smallest, it turns out.

--- test/testrunner.sh
+++ test/testrunner.sh
@@ -6,7 +6,7 @@ DIR=$1
 PYDRIVER=../lzjb.py
 CDRIVER=./cdriver
 
-find $DIR -maxdepth 1  -type f  \! -name '.*'  -readable | head | xargs ls -rS | while IFS= read -r f; do
+find $DIR -maxdepth 1  -type f  \! -name '.*'  -readable | xargs ls -S | head | while IFS= read -r f; do
 	./testfile.sh $PYDRIVER $PYDRIVER $f
 	./testfile.sh $PYDRIVER $CDRIVER  $f
 	./testfile.sh $CDRIVER  $PYDRIVER $f
